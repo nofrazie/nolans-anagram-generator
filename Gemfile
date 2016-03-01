237a6a1816f60637c87f360281b22c0ae6d19813
Nolan Frazier <nofrazier3@gmail.com>
2016-02-29 19:43:09 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rspec'
+gem 'activerecord'
+gem 'pg'
+
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'rake'
+gem 'rack'
+gem 'rack-test'
+gem 'rack-env'
+
+gem 'puma'
+
+ruby '2.3.0'
---
+++ app/models/word.rb
@@ -0,0 +1,18 @@
+class Word
+  def self.find_anagrams(string)
+    string.split("").permutation.map {|i| i.join}
+  end
+
+  def self.reverse_letters(letters)
+    # create a new array of 2 items
+    length = letters.length
+    reversed_letters = Array.new(length)
+
+    # loop through letters and keep index
+    letters.each_with_index do |letter, index|
+      reversed_letters[length - index - 1] = letter
+    end
+
+    reversed_letters
+  end
+end
