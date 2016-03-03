6df6449c42f2c65986e035fd2179bc9ea1f36ad4
Nolan Frazier <nofrazier3@gmail.com>
2016-03-02 16:28:15 -0800

    Added error methods and views for words longer than 3 letters and repeating letters

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,13 +2,48 @@ get '/' do
   erb :index
 end
 
+post '/' do
+
+  def three_letters?(input)
+    if input.length < 4
+      return true
+    else
+      return false
+    end
+  end
+
+  def distinct_letters?(input)
+    letter_array = input.chars
+    unique_letters = letter_array.uniq
+    if unique_letters.length < letter_array.length
+      false
+    else
+      true
+    end
+  end
+
+  def valid_input?(input)
+    if three_letters?(input) && distinct_letters?(input)
+      true
+    else
+      false
+    end
+  end
+
+  word = params[:word]
+
+  if valid_input?(word)
+    redirect "/anagrams/#{word}"
+  else
+    @error = "Your word is either longer than 3 letters or doesn't use distinct letters. Please enter a new word"
+    erb :index
+  end
+
+end
+
+
 get '/anagrams/:word' do
   @word = params[:word]
   @anagrams = Word.find_anagrams(@word)
   erb :show
-end
-
-post '/' do
-  @word = params[:word]
-  redirect "/anagrams/#{@word}"
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -13,11 +13,9 @@ class Word < ActiveRecord::Base
   end
 
   def self.reverse_letters(letters)
-    # create a new array of 2 items
     length = letters.length
     reversed_letters = Array.new(length)
 
-    # loop through letters and keep index
     letters.each_with_index do |letter, index|
       reversed_letters[length - index - 1] = letter
     end
